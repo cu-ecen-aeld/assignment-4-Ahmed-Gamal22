232c29030498837cd75b3de17b116fcbafc72f21
Ahmed Gamal <ahmedgamalahmed1994@gmail.com>
2026-08-02 16:01:47 +0300

    update another hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 82ca84372eeacceda0a4393db44f6766334f7267
+AESD_ASSIGNMENTS_VERSION = d52670732e1edc620343fbe10c2e65009ea88b70
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
